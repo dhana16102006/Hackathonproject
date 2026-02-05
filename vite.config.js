619c3e554916a5f6bf0529694bc94bc6f7bfd959
dhana lakshmi <dhana16102006lakshmi@gmail.com>
2026-02-05 22:59:16 +0530

    Updated UI / fixed bug / new feature

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "My Note App",
+        short_name: "MyNote",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff"
+      }
+    })
+  ]
+});
